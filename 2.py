5140ade7ca60cec1c091ce4cb829851e98e479b7
gor-dimm <booo130502@gmail.com>
2021-03-25 16:54:54 +0300

    Задание 2

---
+++ 2.py
@@ -0,0 +1,11 @@
+a = input("Введите первое число: ")
+b = input("Введите второе число: ")
+c = input("Введите третье число: ")
+print(min(a,b,c),(max(min(a,b), min(b,c))
+                  if not
+min(a,b) == min(b,c)
+                  else min(a,c)), max(a,b,c))
+print(max(a,b,c),(min(max(a,b), max(b,c))
+                  if not
+max(a,b) == max(b,c)
+                  else max(a,c)), min(a,b,c))
