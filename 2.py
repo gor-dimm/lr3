b352d291835cccc30487aebb0e0ef6ccad04e495
gor-dimm <booo130502@gmail.com>
2021-04-07 19:21:21 +0300

    Пример 2

--- 2.py
+++ 2.py
@@ -1,11 +1,15 @@
+# Составить UML-диаграмму деятельности и программу для решения задачи: с
+# клавиатуры вводится номер месяца от 1 до 12, необходимо для этого номера месяца
+# вывести наименование времени года.
+
 a = input("Введите первое число: ")
 b = input("Введите второе число: ")
 c = input("Введите третье число: ")
-print(min(a,b,c),(max(min(a,b), min(b,c))
-                  if not
-min(a,b) == min(b,c)
-                  else min(a,c)), max(a,b,c))
-print(max(a,b,c),(min(max(a,b), max(b,c))
-                  if not
-max(a,b) == max(b,c)
-                  else max(a,c)), min(a,b,c))
+print(min(a, b, c), (max(min(a, b), min(b, c))
+                     if not
+min(a, b) == min(b, c)
+                     else min(a, c)), max(a, b, c))
+print(max(a, b, c), (min(max(a, b), max(b, c))
+                     if not
+max(a, b) == max(b, c)
+                     else max(a, c)), min(a, b, c))
